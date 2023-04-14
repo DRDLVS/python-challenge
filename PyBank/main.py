0540ba4eb9a6365c98b2d1006a0f063fe79b1f38
Diego Rodriguez de la Vega Sanz <diego.rdlvs1@gmail.com>
2023-04-14 17:58:38 -0600

    Start PyBank Code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,10 @@
 import csv
+
+
+with open ("PyBank/Resources/budget_data.csv",mode="r") as csvfile:
+   # lines = csvfile.readlines()
+   reader = csv.reader(csvfile)
+   next (reader)
+   for line in reader:
+       print(line[0], line[1])
+    
