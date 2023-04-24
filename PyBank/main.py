f5b41eb40a8945d3a340e66552c1511307c2f342
Diego Rodriguez de la Vega Sanz <diego.rdlvs1@gmail.com>
2023-04-24 17:31:03 -0600

    Final commit

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,10 +1,70 @@
+import os
 import csv
+from itertools import count
+
+input_file = os.path.join("PyBank/Resources/budget_data.csv")
+output_file = os.path.join("PyBank/analysis/budget_analysis.txt")
+
+total_months = 0
+total_budget = 0
+change_list = []
+greatest_increase = ["",0]
+greatest_decrease = ["",9999]
 
 
-with open ("PyBank/Resources/budget_data.csv",mode="r") as csvfile:
+with open (input_file) as csvfile:
    # lines = csvfile.readlines()
+
+
    reader = csv.reader(csvfile)
-   next (reader)
+
+   header_row = next (reader)
+   first_data_row = next (reader)
+   previous_value = int(first_data_row[1])
+   total_months +=1 
+   total_budget += int(first_data_row[1])
+
+   
    for line in reader:
-       print(line[0], line[1])
+       total_budget += int(line[1])
+       current_value = int(line[1])
+       change = current_value - previous_value
+       change_list.append(change)
+       previous_value = current_value
+       
+       
+       total_months +=1   
+
+       if change > greatest_increase[1]:
+           greatest_increase[1] = change
+           greatest_increase[0] = line[0]
+
+       if change < greatest_decrease[1]:
+           greatest_decrease[1] = change
+           greatest_decrease[0] = line[0]
+
+average_change = round(sum(change_list)/len(change_list),2)
+max_change = change_list.index(max(change_list))
+min_change = change_list.index(min(change_list))
+
+output = (
+    f"Financial Analysis\n"
+    f"--------------------\n"
+    f"Total Months: {total_months}\n"
+    f"Total: ${total_budget}\n"
+    f"Average Change: ${average_change}\n"
+    f"Greatest Increase in Profits: ${greatest_increase}\n"
+    f"Greatest Decrease in Profits: ${greatest_decrease}\n"
+
+) 
+
+print(output)
+
+with open(output_file,"w") as txt_out:
+    txt_out.write(output)
+
+
+
+
     
+        
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,58 @@
+import os
 import csv
+from itertools import count
+
+input_file = os.path.join("PyPoll/Resources/election_data.csv")
+output_file = os.path.join("PyPoll/analysis/election_analysis.txt")
+
+
+with open(input_file) as csvfile:
+    reader = csv.reader(csvfile)
+
+    next(reader)
+    data = list(reader)
+    row_count = len(data)
+
+    candidates_list = list()
+    tally = list()
+    for i in range(0, row_count):
+        candidate = data[i][2]
+        tally.append(candidate)
+        if candidate not in candidates_list:
+            candidates_list.append(candidate)
+    candicount = len(candidates_list)
+
+    votes = list()
+    percentage = list()
+    for s in range(0, candicount):
+        name = candidates_list[s]
+        votes.append(tally.count(name))
+        vprct = votes[s]/row_count
+        percentage.append(vprct)
+
+    winner = votes.index(max(votes))
+
+with open(output_file,"w") as txtfile:
+    output = (
+        f"Election Results\n"
+        f"----------------------------\n"
+        f"Total Votes: {row_count:}\n"
+        f"----------------------------\n")
+    print(output)
+    txtfile.write(output)
+
+
+    for k in range(0, candicount):
+        output_candidate = f"{candidates_list[k]}: {percentage[k]:.3%} ({votes[k]:,})\n"
+        print(output_candidate)
+        txtfile.write(output_candidate)
+
+
+    output = (
+    f"----------------------------\n"
+    f"Winner: {candidates_list[winner]}\n"
+    f"----------------------------\n")
+
+    print(output)
+    txtfile.write(output)
+
